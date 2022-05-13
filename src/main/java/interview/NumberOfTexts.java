be2a5d890df7c45157f62b6032e8b44aeb652af3
Vlad Nitu <V.P.Nitu@student.tudelft.nl>
2022-05-13 22:54:54 +0200

    "NumberOfTexts" wekekly contest LeetCode DP solution
    Took 20 minutes

---
+++ src/main/java/interview/NumberOfTexts.java
@@ -0,0 +1,43 @@
+package interview;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// LeetCode link: https://leetcode.com/problems/count-number-of-texts/
+// O(N) time complexity, O(N) space complexity
+// Dynamic Programming
+public class NumberOfTexts {
+
+        private static int MOD = 1000000007;
+        private static List<Character> edgeCases;
+
+    public static int countTexts(String pressedKeys) {
+
+        edgeCases = new ArrayList<>();
+        edgeCases.add('7');
+        edgeCases.add('9');
+
+        int N = pressedKeys.length();
+        int[] dp = new int[N+1]; //dp[i] -> # of texts that can be sent having 'i' characters
+        dp[0] = 1; // Empty string
+
+        for (int i = 1; i <= N; ++i){
+            char c = pressedKeys.charAt(i - 1);
+            dp[i] = dp[i-1] % MOD;
+
+            if (i >= 2 && c == pressedKeys.charAt(i-2)) // 2 consecutive digits
+                dp[i] = (dp[i] + dp[i-2]) % MOD;
+            else continue; // skip to the next loop
+
+            if (i >= 3 && c == pressedKeys.charAt(i-3)) // 3 consecutive digits
+                dp[i] = (dp[i] + dp[i-3]) % MOD;
+            else continue; // skip to the next loop
+
+            if (i >= 4 && edgeCases.contains(pressedKeys.charAt(i-4)) && c == pressedKeys.charAt(i-4)) // 4 consecutive digits, that may be only 7 or 9
+                dp[i] = (dp[i] + dp[i-4]) % MOD;
+        }
+
+
+            return dp[N];
+    }
+}
---
+++ src/test/java/interview/NumberOfTextsTest.java
@@ -0,0 +1,30 @@
+package interview;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class NumberOfTextsTest {
+
+    @ParameterizedTest
+    @MethodSource("generate")
+    void testCases(String pressedKeys, int expected) {
+        assertThat(NumberOfTexts.countTexts(pressedKeys))
+                .isEqualTo(expected);
+    }
+
+    static Stream<Arguments> generate() {
+
+        return Stream.of(
+                // Tests provided during LeetCode 292 weekly contest: T1 and T2
+                Arguments.of("22233", 8), //T1
+                Arguments.of("222222222222222222222222222222222222", 82876089), //T2
+                Arguments.of("444479999555588866", 3136) //T3, edge case: 7 is 4 positions before 9, but 7 != 9
+        );
+
+    }
+}
